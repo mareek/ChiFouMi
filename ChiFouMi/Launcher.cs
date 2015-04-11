ea66c87f6b4c0c65d6130fdeea93aa17f1345612
Matthieu Mourisson <mareek@gmail.com>
2015-04-11 15:20:58 +0200

    Encapsulated the game launching mechanic into a launcher class

---
+++ ChiFouMi/Launcher.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ChiFouMi
+{
+    public static class Launcher
+    {
+        private const int MaxInputForClassicGame = 3;
+        private const int MaxInputForLeonardNimoy = 5;
+
+        public static void LaunchPlayerVsComputerClassicGame(bool roxorMode)
+        {
+            LaunchPlayerVsComputerGame(roxorMode, MaxInputForClassicGame);
+        }
+
+        public static void LaunchPlayerVsComputerSpockGame(bool roxorMode)
+        {
+            LaunchPlayerVsComputerGame(roxorMode, MaxInputForLeonardNimoy);
+        }
+
+        public static void LaunchPlayerVsComputerGame(bool roxorMode, int maxInput)
+        {
+            var rand = new Random();
+
+            Func<string> inputPlayer = Console.ReadLine;
+            Func<int> inputComputer = () => rand.Next(1, maxInput + 1);
+
+            new Game(Console.WriteLine, maxInput).PlayGame(roxorMode, inputPlayer, inputComputer);
+        }
+    }
+}
--- ChiFouMi/Program.cs
+++ ChiFouMi/Program.cs
@@ -1,21 +1,13 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 
 namespace ChiFouMi
 {
     class Program
     {
-        private const int MaxInput = 5;
-
         static void Main(string[] args)
         {
-            var rand = new Random();
-
-            Func<string> inputPlayer = Console.ReadLine;
-            Func<int> inputComputer = () => rand.Next(1, MaxInput + 1);
             var roxorMode = args.FirstOrDefault() == "roxor";
-
-            new Game(Console.WriteLine, MaxInput).PlayGame(roxorMode, inputPlayer, inputComputer);
+            Launcher.LaunchPlayerVsComputerSpockGame(roxorMode);
         }
     }
 }
